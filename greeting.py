f4949ace33240aa4a4a27ac4bbc7f2d5499a4dc5
bottleofrye <144550037+bottleofrye@users.noreply.github.com>
2023-09-09 14:35:05 -0400

    Create greeting.py

---
+++ greeting.py
@@ -0,0 +1,11 @@
+# This Python script calculates the area of a rectangle.
+
+# Prompt the user for the length and width of the rectangle
+length = float(input("Please enter the length of the rectangle: "))
+width = float(input("Please enter the width of the rectangle: "))
+
+# Calculate the area of the rectangle
+area = length * width
+
+# Display the result
+print("The area of the rectangle is:", area)
